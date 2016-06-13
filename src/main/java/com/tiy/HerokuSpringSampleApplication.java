374f69f647699c26630d2551c36fff05b1f15e1e
Dominique Bashizi <dominique@blupods.com>
2016-06-13 10:54:36 -0400

    Testing JDBC settings

--- src/main/java/com/tiy/HerokuSpringSampleApplication.java
+++ src/main/java/com/tiy/HerokuSpringSampleApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HerokuSpringSampleApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("**********************************************"); 
+		System.out.println("JDBC_DATABASE_URL = " + System.getenv("JDBC_DATABASE_URL")); 
+		System.out.println("DATABASE_URL = " + System.getenv("DATABASE_URL")); 
+		System.out.println("**********************************************"); 
+
 		SpringApplication.run(HerokuSpringSampleApplication.class, args);
 	}
 }
